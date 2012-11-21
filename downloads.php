f6c60a6d8adaa306b51f9846d538bae8087d13ed
Jeff Coburn <jmc.junk@gmail.com>
2012-11-21 12:36:51 -0500

    li bug

--- downloads.php
+++ downloads.php
@@ -13,7 +13,7 @@
           <li><a href="files/Data Dictionary_VR_4.0.pdf">Vocational Rehabilitation Data Dictionary</a> (pdf) -updated 6/12</li>
           <li>2011 National Survey of State Vocational Rehabilitation Agencies (<a href="files/VRSurvey_ExploreVR.xlsx">Excel</a>, <a href="files/VRSurvey_ExploreVR.csv">CSV</a>)</li>
           <li><a href="files/Welfare_datadictionary_5.0.pdf">Welfare Data Dictionary</a></li>
-         < li>The 2011 Survey of State Welfare Agencies (<a href="files/Welfare_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/Welfare_Survey_ExploreVR.csv">CSV</a>)</li>
+         <li>The 2011 Survey of State Welfare Agencies (<a href="files/Welfare_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/Welfare_Survey_ExploreVR.csv">CSV</a>)</li>
           
           </ul>
 </article>
--- index.php
+++ index.php
@@ -49,18 +49,8 @@
         <p><a class="btn" href="cd.php">View collaboration data &raquo;</a></p>
                </div>
                 <div class="span4">
-                   <h2>Downloads</h2>
-          <ul>
-            <li><a href="files/FactSheet_exploreVRlink.pdf">Fact Sheet</a> (Fall 2012)</li>
-          <li><a href="files/Data Dictionary_MH_6.0.pdf">Mental Health Data Dictionary</a> (pdf) -updated 10/12</li>
-          <li>2011 Survey of State Mental Health Agencies (<a href="files/MH_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/MH_Survey_ExploreVR.csv">CSV</a>)</li>
-           <li><a href="files/Data Dictionary_VR_4.0.pdf">Vocational Rehabilitation Data Dictionary</a> (pdf) -updated 6/12</li>
-           <li>2011 National Survey of State Vocational Rehabilitation Agencies (<a href="files/VRSurvey_ExploreVR.xlsx">Excel</a>, <a href="files/VRSurvey_ExploreVR.csv">CSV</a>)</li>
-
-            <li><a href="files/Welfare_datadictionary_5.0.pdf">Welfare Data Dictionary</a></li>
-            <li>The 2011 Survey of State Welfare Agencies (<a href="files/Welfare_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/Welfare_Survey_ExploreVR.csv">CSV</a>)</li>
-          
-          </ul>
+                   <h2>Explore Findings</h2>
+           <p><a class="btn" href="review_vr.php">Business Employment Relations Specialists &raquo;</a></p>
                 </div>
             </div>
 
