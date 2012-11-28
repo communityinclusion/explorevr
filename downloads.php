b9bbd3674cb04419c4185a805378ad75db5af50c
Kartik Trivedi <kartik0trivedi@gmail.com>
2012-11-28 15:54:30 -0500

    Made changes based on HJ suggestions

--- downloads.php
+++ downloads.php
@@ -2,19 +2,19 @@
 
 <article>
 			<h1>Downloads</h1>
-<h2>Explore Data</h2>
+
 <p>On this page you will find various items available for download. We offer access to full datasets from the ICI as well as accompanying data dictionaries so you can make full use of the raw data for your own analyses. We also offer PDFs of materials we have created using data from the ICI and RSA. Please check back often as we are continually updating and adding resources to this page.</p>
 
 
 <ul>
-          <li><a href="files/FactSheet_exploreVRlink.pdf">Fact Sheet</a> (Fall 2012)</li>
-          <li><a href="files/Data Dictionary_MH_6.0.pdf">Mental Health Data Dictionary</a> (pdf) -updated 10/12</li>
+          <li>Fact Sheet - updaates 10/2012 (<a href="files/FactSheet_exploreVRlink.pdf">PDF</a>)</li>
           <li>2011 Survey of State Mental Health Agencies (<a href="files/MH_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/MH_Survey_ExploreVR.csv">CSV</a>)</li>
-          <li><a href="files/Data Dictionary_VR_4.0.pdf">Vocational Rehabilitation Data Dictionary</a> (pdf) -updated 6/12</li>
+          <li>2011 Survey of State Mental Health Agencies Data Dictionary - updated 10/2012 (<a href="files/Data Dictionary_MH_6.0.pdf">PDF</a>)</li>
           <li>2011 National Survey of State Vocational Rehabilitation Agencies (<a href="files/VRSurvey_ExploreVR.xlsx">Excel</a>, <a href="files/VRSurvey_ExploreVR.csv">CSV</a>)</li>
-          <li><a href="files/Welfare_datadictionary_5.0.pdf">Welfare Data Dictionary</a></li>
-          <li>The 2011 Survey of State Welfare Agencies (<a href="files/Welfare_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/Welfare_Survey_ExploreVR.csv">CSV</a>)</li>
-          <li><a href="files/ExploreVR_TAY.pdf">The Rehabilitation Rates for General and Combined VR Agencies</a> (pdf) -updated 11/15 </li>
+          <li>2011 National Survey of State Vocational Rehabilitation Agencies Data Dictionary - updated 06/2012 (<a href="files/Data Dictionary_VR_4.0.pdf">PDF</a>)</li>
+          <li>2011 Survey of State Welfare Agencies (<a href="files/Welfare_Survey_ExploreVR.xlsx">Excel</a>, <a href="files/Welfare_Survey_ExploreVR.csv">CSV</a>)</li>
+          <li>2011 Survey of State Welfare Agencies Data Dictionary (<a href="files/Welfare_datadictionary_5.0.pdf">PDF</a>)</li>
+
           </ul>
 </article>
 
