d296fc64039bb37f63a02c4035f80bec257befab
Jeff Coburn <jmc.junk@gmail.com>
2012-12-05 13:35:55 -0500

    tracking for pdfs

--- after.php
+++ after.php
@@ -24,6 +24,13 @@
   })();
 
 </script>
+<script>
+    $("a[href$='pdf']").each(function(index) {
+      pdfLabel = $(this).attr('href');
+      pdfOnClick = "_gaq.push(['_trackEvent', 'PDF', 'Download', '" + pdfLabel + "']);";
+      $(this).attr("onClick", pdfOnClick);
+    });
+</script>
        <a href="https://github.com/communityinclusion/explorevr" id="github"><img style=" z-index:10000; position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a>
     </body>
 </html>
