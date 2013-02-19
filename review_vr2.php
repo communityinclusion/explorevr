b122b75233a64d41fd3e49b06a3bcd10bfda41f9
Jeff Coburn <jmc.junk@gmail.com>
2013-02-19 10:22:30 -0500

    new review VR et changes to old

---
+++ review_vr2.php
@@ -0,0 +1,20 @@
+<?php include_once "before.php"; ?>
+
+
+<h1>The Deciding Factor: Making Decisions on Management Information Systems and Data Use in State Vocational Rehabilitation Agencies</h1>
+<h2>Review VR Issue 2, 2012</h2>
+
+<p>Authors: Elizabeth Porter, Dana Jefferson, Susan Foley, &amp; Ngai Kwan</p>
+
+<p>In 2011, the VR-RRTC <a href="www.VR-RRTC.org">http://www.vr-rrtc.org</a> surveyed all 80 state vocational rehabilitation agencies (SVRAs) in the nation to collect descriptive information about structure, processes, and partnerships. Among many topics, a series of questions asked SVRAs about decision-making authority for selected business functions. The questions were designed to determine whether SVRAs had ultimate control over their Management Information Systems (MIS), or whether they had to work with another agency to obtain approval for any changes related to a specific part of their MIS.</p>
+
+<p>SVRAs were able to indicate whether the Designated State Unit (DSU); the Designated State Authority (DSA), if different from the DSU; or another state unit or entity was the primary decision maker. This brief describes decision-making authority over MIS functions. Out of the 71 reporting VR agencies, 46 reported that the DSU and the DSA were different in their state, while 25 indicated that the DSU and the DSA were the same.</p>
+
+<p><strong>MIS decision-making in SVRAs in which the DSU is different from the DSA:</strong> Nearly half (21) of the 46 SVRAs indicated that primary decision-making authority on MIS hardware and software was with the DSA or another entity in the state government. However, 42 out of 46 SVRAs indicated that they had control over decisions on data analysis and use.</p>
+
+<p><strong>MIS decision-making in SVRAs in which the DSU is the same as the DSA:</strong> Eighteen of the 25 SVRAs reported that the DSU was the primary decision maker regarding MIS hardware and software. All 25 agencies reported that the DSU was the primary decision maker for data analysis and use.</p>
+
+<p><strong>Discussion:</strong> More than one third of responding SVRAs do not have primary decision-making authority over MIS hardware and software. This may create implications for investing in electronic case management software, quality assurance and performance management systems, interagency data-sharing options, real-time labor market information systems, business intelligence systems and dashboards, and partnerships with researchers. Changes to required data collection (such as case closure information for RSA 911) might require more time and interagency coordination in SVRAs without decision-making authority than in SVRAs with decision-making authority. In future iterations of the VR surveys, the VR-RRTC will be documenting investment in the systems listed above.</p>
+
+
+<?php include_once "after.php"; ?>
