3f34f81a905aa6840304944345fe6fe448d23745
Jeff Coburn <jmc.junk@gmail.com>
2013-02-23 16:18:56 -0500

    addend review vr2

--- review_vr2.php
+++ review_vr2.php
@@ -6,6 +6,8 @@
 
 <p>Authors: Elizabeth Porter, Dana Jefferson, Susan Foley, &amp; Ngai Kwan</p>
 
+<span class="left"><a href="files/review_vr/ReviewVR_2.pdf"><img src="img/ReviewVR_2_.png"><p class=""><br /><b><i class="icon-download"></i>Download PDF</b></p></a></span>
+
 <p>In 2011, the VR-RRTC <a href="www.VR-RRTC.org">http://www.vr-rrtc.org</a> surveyed all 80 state vocational rehabilitation agencies (SVRAs) in the nation to collect descriptive information about structure, processes, and partnerships. Among many topics, a series of questions asked SVRAs about decision-making authority for selected business functions. The questions were designed to determine whether SVRAs had ultimate control over their Management Information Systems (MIS), or whether they had to work with another agency to obtain approval for any changes related to a specific part of their MIS.</p>
 
 <p>SVRAs were able to indicate whether the Designated State Unit (DSU); the Designated State Authority (DSA), if different from the DSU; or another state unit or entity was the primary decision maker. This brief describes decision-making authority over MIS functions. Out of the 71 reporting VR agencies, 46 reported that the DSU and the DSA were different in their state, while 25 indicated that the DSU and the DSA were the same.</p>
@@ -14,7 +16,11 @@
 
 <p><strong>MIS decision-making in SVRAs in which the DSU is the same as the DSA:</strong> Eighteen of the 25 SVRAs reported that the DSU was the primary decision maker regarding MIS hardware and software. All 25 agencies reported that the DSU was the primary decision maker for data analysis and use.</p>
 
+<div><iframe width="760px" title="Column Chart: Review VR No 2, Figure 1" height="646px" src="https://opendata.socrata.com/w/ayvi-3yb6/y34g-bnf3?cur=FtnGD0XfbE1&from=root" frameborder="0" scrolling="no"><a href="https://opendata.socrata.com/dataset/Column-Chart-Review-VR-No-2-Figure-1/ayvi-3yb6" title="Column Chart: Review VR No 2, Figure 1" target="_blank">Column Chart: Review VR No 2, Figure 1</a></iframe><p><a href="http://www.socrata.com/" target="_blank">Powered by Socrata</a></p></div>
+
 <p><strong>Discussion:</strong> More than one third of responding SVRAs do not have primary decision-making authority over MIS hardware and software. This may create implications for investing in electronic case management software, quality assurance and performance management systems, interagency data-sharing options, real-time labor market information systems, business intelligence systems and dashboards, and partnerships with researchers. Changes to required data collection (such as case closure information for RSA 911) might require more time and interagency coordination in SVRAs without decision-making authority than in SVRAs with decision-making authority. In future iterations of the VR surveys, the VR-RRTC will be documenting investment in the systems listed above.</p>
+
+<div><iframe width="760px" title="Column chart: ReviewVR2, Figure 2" height="646px" src="https://opendata.socrata.com/w/grth-xtfu/y34g-bnf3?cur=aXeaY1Y_FgE&from=root" frameborder="0" scrolling="no"><a href="https://opendata.socrata.com/dataset/Column-chart-ReviewVR2-Figure-2/grth-xtfu" title="Column chart: ReviewVR2, Figure 2" target="_blank">Column chart: ReviewVR2, Figure 2</a></iframe><p><a href="http://www.socrata.com/" target="_blank">Powered by Socrata</a></p></div>
 
 
 <?php include_once "after.php"; ?>
